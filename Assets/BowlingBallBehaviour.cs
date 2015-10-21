489071ed7371ab84bd2973a1a51d9bdc359bc1ca
unknown <ostrowskirafal1990@gmail.com>
2015-10-21 22:03:08 +0200

    Lab no. 2 - keyboard and mouse events for the main scene and a bowling ball + measuring time.

--- Assets/BowlingBallBehaviour.cs
+++ Assets/BowlingBallBehaviour.cs
@@ -1,34 +1,100 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
-public class BowlingBallBehaviour : MonoBehaviour {
-	public bool  IsForwardPressed { get; set; }
+/// <summary>
+/// Responsible for bowling ball's behaviour, f.e. its movement.
+/// Keys:
+/// r - reset ball's position.
+/// w - move ball forward
+/// Mouse buttons:
+/// left key pressed and released - logs time for how long it was pressed.
+/// </summary>
+public class BowlingBallBehaviour : MonoBehaviour
+{
+    private const int MouseLeftKey = 0;
+    private const string ForwardKey = "w", ResetBallPositionKey = "r";
+    private const float BallSpeed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButtonDown(0)) {
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if(Physics.Raycast(ray, out hit)) {
-				gameObject.GetComponent<Renderer>().material.color = Color.red;
-			}
-		}
-		
-		if (Input.GetKeyDown("w")) {
-			 IsForwardPressed = true;
-		}
-		
-		if (Input.GetKeyUp("w"))  {
-			IsForwardPressed = false;
-		}
-		
-		if (IsForwardPressed) {
-			transform.Translate(Vector3.back * Time.deltaTime);
-		}
-	}
+    private int _mouseClickedTime;
+    private Vector3 _objectStartPosition;
+
+    public bool IsForwardPressed { get; set; }
+    public int MouseClickTimeInMiliseconds { get; set; }
+
+    // Use this for initialization
+    private void Start()
+    {
+        _objectStartPosition = GameObject.Find("Sphere").transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(ForwardKey))
+        {
+            IsForwardPressed = true;
+        }
+
+        if (Input.GetKeyUp(ForwardKey))
+        {
+            IsForwardPressed = false;
+        }
+
+        if (Input.GetKeyDown(ResetBallPositionKey))
+        {
+            ResetBallPosition();
+        }
+
+        if (IsForwardPressed)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * BallSpeed);
+        }
+
+        if (LeftMouseWasClicked())
+        {
+            StartCountingHowLongMouseIsPressed();
+        }
+
+        if (LeftMouseWasReleased())
+        {
+            StopCountingHowLongMouseIsPressed();
+            LogForHowLongMouseWasPressed();
+        }
+    }
+
+    private void ResetBallPosition()
+    {
+        GameObject.Find("Sphere").transform.position = _objectStartPosition;
+    }
+
+    private void StartCountingHowLongMouseIsPressed()
+    {
+        MouseClickTimeInMiliseconds = 0;
+        _mouseClickedTime = Environment.TickCount;
+    }
+
+    private void StopCountingHowLongMouseIsPressed()
+    {
+        int mouseReleaseTime = Environment.TickCount;
+        MouseClickTimeInMiliseconds = mouseReleaseTime - _mouseClickedTime;
+    }
+
+
+    /// <summary>
+    /// Just temporarily, to show that it was measured properly. 
+    /// </summary>
+    private void LogForHowLongMouseWasPressed()
+    {
+        Debug.Log(string.Format("Left mouse button was pressed for: {0} ms", MouseClickTimeInMiliseconds));
+    }
+
+    private bool LeftMouseWasClicked()
+    {
+        return Input.GetMouseButtonDown(MouseLeftKey);
+    }
+
+    private bool LeftMouseWasReleased()
+    {
+        return Input.GetMouseButtonUp(MouseLeftKey);
+    }
 }
--- Assets/MainSceneDisplay.cs
+++ Assets/MainSceneDisplay.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Random = UnityEngine.Random;
 
-public class MainSceneDisplay : MonoBehaviour {
+/// <summary>
+/// Changes the look of the main scene - f.e. sets its default color and allows to set a random color.
+/// Interacts with player's mouse and keyboard.
+/// Keys:
+/// k - allows to randomize floor's color
+/// Mouse buttons:
+/// right key - allows to randomize floor's color
+/// </summary>
+public class MainSceneDisplay : MonoBehaviour
+{
+    private const int MouseRightKey = 1;
+    private const string ChangeColorKey = "k";
+    private readonly Color _defaultColor = Color.black;
 
-	// Use this for initialization
-	void Start () {
-		 gameObject.GetComponent<Renderer>().material.color = Color.black;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Use this for initialization
+    private void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.color = _defaultColor;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (RightMouseWasClicked() || ChangeColorKeyWasPressed())
+        {
+            ChangeSceneColorToRandom();
+        }
+    }
+
+    /// <summary>
+    /// Detects if mouse's right button was clicked.
+    /// </summary>
+    /// <returns>True if the right key was pressed.</returns>
+    private bool RightMouseWasClicked()
+    {
+        return Input.GetMouseButtonDown(MouseRightKey);
+    }
+
+    /// <summary>
+    /// Detects if key that is responsible for changing the color was pressed on a keyboard.
+    /// </summary>
+    /// <returns>True if the key responsible was changing color was pressed.</returns>
+    private bool ChangeColorKeyWasPressed()
+    {
+        return Input.GetKeyUp(ChangeColorKey);
+    }
+
+    /// <summary>
+    /// Generates a random color, which is then set for the main scene.
+    /// </summary>
+    private void ChangeSceneColorToRandom()
+    {
+        var randomColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+        gameObject.GetComponent<Renderer>().material.color = randomColor;
+    }
 }
