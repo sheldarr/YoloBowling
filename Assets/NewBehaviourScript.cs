972bb68e25ca5d4fa197ac101e1fa1151294149d
sheldar@wp.pl <sheldar@wp.pl>
2015-11-11 17:52:29 +0100

    Added uniform square motion.

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LinearMoveBehaviour : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/UniformSquareMotion.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class UniformSquareMotion : MonoBehaviour
+{
+    private const float MinSpeed = 1;
+    private const float MaxSpeed = 3;
+    private const float DefaultTimeToChangeDirection = 5;
+
+    private Vector3 _currentDirection;
+    private float _timeToChangeDirection;
+    private float _speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _currentDirection = new Vector3 { x = Random.value, y = 0, z = Random.value }.normalized;
+        _timeToChangeDirection = DefaultTimeToChangeDirection;
+        _speed = Random.Range(MinSpeed, MaxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_currentDirection * Time.deltaTime * _speed);
+        _timeToChangeDirection -= Time.deltaTime;
+
+        if (_timeToChangeDirection < 0)
+        {
+            RotateBy90Degrees();
+            _timeToChangeDirection = DefaultTimeToChangeDirection;
+        }
+    }
+
+    private void RotateBy90Degrees()
+    {
+        _currentDirection = Quaternion.Euler(0, 90, 0) * _currentDirection;
+    }
+}
