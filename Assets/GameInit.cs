4079a69ce13831705e25bb022ede06edcdc5db04
sheldar@wp.pl <sheldar@wp.pl>
2015-12-09 17:55:03 +0100

    Passing reference to GameInit script by Unity

--- Assets/GameInit.cs
+++ Assets/GameInit.cs
@@ -3,24 +3,38 @@
 /// <summary>
 /// Created: 10/11/2015
 /// CreatedBy: Kewin Polok
-/// LastModified: 11/11/2015
+/// LastModified: 09/12/2015
 /// LastModifiedBy: Kewin Polok
 /// Description: Game initialization script. At this moment it only sets different material color for pins with different motion.
 /// </summary>
-public class GameInit : MonoBehaviour {
+public class GameInit : MonoBehaviour
+{
+    public GameObject Pin1;
+    public GameObject Pin2;
+    public GameObject Pin3;
+    public GameObject Pin4;
+    public GameObject Pin5;
+    public GameObject Pin6;
+    public GameObject Pin7;
+    public GameObject Pin8;
+    public GameObject Pin9;
+    public GameObject Pin10;
+
+    public GameObject BowlingBall;
+
     /// <summary>
     /// Initialization
     /// </summary>
     void Start () {
-        GameObject.Find("Pin_1").GetComponent<Renderer>().material.color = Color.green;
-        GameObject.Find("Pin_2").GetComponent<Renderer>().material.color = Color.green;
-        GameObject.Find("Pin_3").GetComponent<Renderer>().material.color = Color.green;
-        GameObject.Find("Pin_4").GetComponent<Renderer>().material.color = Color.yellow;
-        GameObject.Find("Pin_5").GetComponent<Renderer>().material.color = Color.yellow;
-        GameObject.Find("Pin_6").GetComponent<Renderer>().material.color = Color.yellow;
-        GameObject.Find("Pin_7").GetComponent<Renderer>().material.color = Color.red;
-        GameObject.Find("Pin_8").GetComponent<Renderer>().material.color = Color.red;
-        GameObject.Find("Pin_9").GetComponent<Renderer>().material.color = Color.red;
-        GameObject.Find("Pin_10").GetComponent<Renderer>().material.color = Color.red;
+        Pin1.GetComponent<Renderer>().material.color = Color.green;
+        Pin2.GetComponent<Renderer>().material.color = Color.green;
+        Pin3.GetComponent<Renderer>().material.color = Color.green;
+        Pin4.GetComponent<Renderer>().material.color = Color.yellow;
+        Pin5.GetComponent<Renderer>().material.color = Color.yellow;
+        Pin6.GetComponent<Renderer>().material.color = Color.yellow;
+        Pin7.GetComponent<Renderer>().material.color = Color.red;
+        Pin8.GetComponent<Renderer>().material.color = Color.red;
+        Pin9.GetComponent<Renderer>().material.color = Color.red;
+        Pin10.GetComponent<Renderer>().material.color = Color.red;
     }
 }
