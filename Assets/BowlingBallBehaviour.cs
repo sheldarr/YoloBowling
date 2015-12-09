b000aa9848d87adb8d9d16b41a0dd4896b7821fa
sheldar@wp.pl <sheldar@wp.pl>
2015-12-09 19:24:22 +0100

    Afterburner implementation

--- Assets/BowlingBallBehaviour.cs
+++ Assets/BowlingBallBehaviour.cs
@@ -18,10 +18,16 @@ public class BowlingBallBehaviour : MonoBehaviour
 
     private const float SideForce = 4.0f;
 
+    private const float MaxAfterBurnerPower = 10;
+
+    private float AfterburnerPower { get; set; }
+
     /// <summary>
     /// The object start position
     /// </summary>
     private Vector3 _startPosition;
+
+    private Quaternion _startRotation;
 
     /// <summary>
     /// Initialization
@@ -29,6 +35,9 @@ public class BowlingBallBehaviour : MonoBehaviour
     private void Start()
     {
         _startPosition = gameObject.transform.position;
+        _startRotation = gameObject.transform.rotation;
+
+        AfterburnerPower = 0f;
     }
 
     /// <summary>
@@ -36,24 +45,80 @@ public class BowlingBallBehaviour : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            gameObject.GetComponent<Rigidbody>().AddTorque(-transform.right * ForwardTorque);
-        }
+        HandleAfterburnerControls();
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            gameObject.GetComponent<Rigidbody>().AddForce(transform.right * SideForce);
-        }
+        HandleAfterburnerColors();
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            gameObject.GetComponent<Rigidbody>().AddForce(-transform.right * SideForce);
-        }
+        HandleMovingControls();
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             ResetBallState();
+        }
+    }
+
+    private void HandleMovingControls()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().AddTorque(-transform.right*ForwardTorque);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().AddTorque(transform.right*ForwardTorque);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.right*SideForce);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(-transform.right*SideForce);
+        }
+    }
+
+    private void HandleAfterburnerColors()
+    {
+        if (AfterburnerPower <= MaxAfterBurnerPower)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (AfterburnerPower <= 0.75*MaxAfterBurnerPower)
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+
+        if (AfterburnerPower <= 0.50*MaxAfterBurnerPower)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (AfterburnerPower <= 0.25*MaxAfterBurnerPower)
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+    private void HandleAfterburnerControls()
+    {
+        if (!Input.GetKey(KeyCode.LeftControl))
+        {
+            if (AfterburnerPower > 0)
+            {
+                Debug.Log(AfterburnerPower);
+                gameObject.GetComponent<Rigidbody>().AddForce(-Vector3.forward*AfterburnerPower, ForceMode.VelocityChange);
+            }
+
+            AfterburnerPower = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.UpArrow) && AfterburnerPower <= MaxAfterBurnerPower)
+        {
+            AfterburnerPower += MaxAfterBurnerPower/100;
         }
     }
 
@@ -63,6 +128,8 @@ public class BowlingBallBehaviour : MonoBehaviour
     private void ResetBallState()
     {
         gameObject.transform.position = _startPosition;
+        gameObject.transform.rotation = _startRotation;
+
         gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
     }
