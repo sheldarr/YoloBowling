2fd1bac20548beedd23bba3aeaa22e06bff5cf51
sheldar@wp.pl <sheldar@wp.pl>
2015-12-09 18:57:10 +0100

    Updated prefabs

--- Assets/PinRespawning.cs
+++ Assets/PinRespawning.cs
@@ -1,12 +1,10 @@
 ﻿using UnityEngine;
-using System.Text.RegularExpressions;
-
 
 /// <summary>
 /// Created: 24/11/2015
 /// CreatedBy: Rafał Ostrowski
-/// LastModified: 24/11/2015
-/// LastModifiedBy: Rafał Ostrowski
+/// LastModified: 09/12/2015
+/// LastModifiedBy: Kewin Polok
 /// Description: Responsible for respawning a pin after it fell off the game floor.
 /// </summary>
 public class PinRespawning : MonoBehaviour
@@ -14,52 +12,32 @@ public class PinRespawning : MonoBehaviour
     /// <summary>
     /// Minimum allowed height.
     /// </summary>
-    private float _minimumAllowedHeight;
-
-    /// <summary>
-    /// Initialize.
-    /// </summary>
-    private void Start()
-    {
-        _minimumAllowedHeight = 2f;
-    }
+    private float _minimumAllowedHeight = -3f;
 
     /// <summary>
     /// Called once per frame.
     /// </summary>
     private void Update()
     {
-        if (transform.position.y < _minimumAllowedHeight)
+        if (!(transform.position.y < _minimumAllowedHeight))
         {
-            Color currentObjectColor = gameObject.GetComponent<Renderer>().material.color;
-            string prefabName = GeneratePrefabName();
-
-            DestroyCurrentObjectAndLogInfo();
-
-            var newPin = Instantiate(Resources.Load(prefabName)) as GameObject;
-            if (newPin != null)
-            {
-                newPin.GetComponent<Renderer>().material.color = currentObjectColor;
-            }
+            return;
         }
+
+        RespawnPin();
+
+        DestroyObject(gameObject);
     }
 
-    /// <summary>
-    /// Generates name for a prefab which will be used to Instantiate new game object.
-    /// </summary>
-    /// <returns>Generated name</returns>
-    private string GeneratePrefabName()
+    private void RespawnPin()
     {
-        string currentPinNumber = Regex.Match(gameObject.name, @"\d+").Value;
-        return "PrefabPin" + currentPinNumber;
-    }
+        var currentObjectColor = gameObject.GetComponent<Renderer>().material.color;
 
-    /// <summary>
-    /// Destroyes current object. Logs info to the console about it.
-    /// </summary>
-    private void DestroyCurrentObjectAndLogInfo()
-    {
-        Destroy(gameObject);
-        Debug.Log(string.Format("Destroying {0}", gameObject.name));
+
+        var newPin = Instantiate(Resources.Load(gameObject.name)) as GameObject;
+        if (newPin != null)
+        {
+            newPin.GetComponent<Renderer>().material.color = currentObjectColor;
+        }
     }
 }
